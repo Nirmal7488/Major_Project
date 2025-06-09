c04040663559f7e6820f50dd0130ed57d308703c
Nirmal Kumar <kumarnirmal0276@gmail.com>
2025-06-09 11:00:25 +0530

    rebuilt pages

--- init/index.js
+++ init/index.js
@@ -1,19 +1,25 @@
 const mongoose = require("mongoose");
-const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+const initData = require("./data.js");
 
-main()
-.then(()=>{console.log("connection successful")})
-.catch((err)=>{console.log(err)})
+const URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+    mongoose.connect(URL)
 }
+
+main().then(()=>{
+    console.log("Connected to DB");
+})
+.catch((err)=>{
+    console.error(err)
+})
 
 const initDB = async ()=>{
     await Listing.deleteMany({});
-    await Listing.insertMany(initData.data);
+    await Listing.insertMany(initData.data)
     console.log("Data was initialized");
+    
 }
 
 initDB();
--- models/listing.js
+++ models/listing.js
@@ -8,7 +8,8 @@ const listingSchema = new mongoose.Schema({
         required:true
     },
     description:{
-        type:String
+        type:String,
+        
     },
     image:{
         type:String,
@@ -16,13 +17,16 @@ const listingSchema = new mongoose.Schema({
         set:(v)=>v===""? "https://images.unsplash.com/photo-1568605114967-8130f3a36994?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v
     },
     price:{
-        type:Number
+        type:Number,
+       
     },
     location:{
-        type:String
+        type:String,
+       
     },
     country:{
-        type:String
+        type:String,
+        
     }
 });
 
